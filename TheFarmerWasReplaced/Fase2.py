9bf7627aef7df041a9393371907c12f3a8ee29b0
Luiz Henrique <luiz.henriqueteixeira2008@gmail.com>
2025-12-13 21:30:16 -0300

    Update Fase2.py
    Corrected the wording in a comment for clarity.

--- TheFarmerWasReplaced/Fase2.py
+++ TheFarmerWasReplaced/Fase2.py
@@ -1,4 +1,4 @@
-# agora com o loop desbloqueado, o while, tirando a colheita manual, porém, colhendo muito rápido, resultando em nenhum feno obtido.
+# agora com o loop desbloqueado, o while, podemos tirar a colheita manual, porém, colhendo muito rápido, resultando em nenhum feno obtido.
 
 # valores True e False, booleanos, são constantes, que nunca mudam no código. resumindo, sim e não.
 while True:
